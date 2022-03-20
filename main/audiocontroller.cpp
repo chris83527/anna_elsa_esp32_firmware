dd8975c00ae8b18b5f7c84caca55c0723ab0e09c
Chris Woods <chris@cmwoods.com>
2022-03-20 16:04:25 +0100

    Mostly working version.

--- main/audiocontroller.cpp
+++ main/audiocontroller.cpp
@@ -38,13 +38,21 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <string>
+#include <cstring>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/unistd.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "driver/i2s.h"
+#include "spiffs_stream.h"
+#include "i2s_stream.h"
+#include "ogg_decoder.h"
+#include "board.h"
+#include "audio_def.h"
+#include "esp_audio.h"
 #include "esp_log.h"
 
 #include "audiocontroller.h"
@@ -70,11 +78,14 @@ void AudioController::initialise() {
     ESP_LOGI(TAG, "Initialising board and codecs");
     I2S_NUM = I2S_NUM_0; // i2s port number
 
-    cfg = DEFAULT_ESP_AUDIO_CONFIG();
+    cfg = DEFAULT_ESP_AUDIO_CONFIG();    
+    cfg.in_stream_buf_size = 10*1024;
+    cfg.out_stream_buf_size = 10*1024;
 
     board_handle = audio_board_init();
     audio_hal_ctrl_codec(board_handle->audio_hal, AUDIO_HAL_CODEC_MODE_DECODE, AUDIO_HAL_CTRL_START);
     cfg.vol_handle = board_handle->audio_hal;
+    cfg.task_prio = 20;
     cfg.vol_set = (audio_volume_set) audio_hal_set_volume;
     cfg.vol_get = (audio_volume_get) audio_hal_get_volume;
     cfg.prefer_type = ESP_AUDIO_PREFER_MEM;
@@ -84,23 +95,22 @@ void AudioController::initialise() {
 
     // Create readers and add to esp_audio
     ESP_LOGI(TAG, "Creating spiffs stream reader...");
-    spiffs_reader = SPIFFS_STREAM_CFG_DEFAULT();
-    spiffs_reader.type = AUDIO_STREAM_READER;
-
+    
+    spiffs_reader = SPIFFS_STREAM_CFG_DEFAULT();    
+    spiffs_reader.type = AUDIO_STREAM_READER;        
 
     esp_audio_input_stream_add(player, spiffs_stream_init(&spiffs_reader));
 
     // Add decoders and encoders to esp_audio
     ESP_LOGI(TAG, "Creating ogg vorbis decoder...");
-    ogg_dec_cfg = DEFAULT_OGG_DECODER_CONFIG();
+    ogg_dec_cfg = DEFAULT_OGG_DECODER_CONFIG();        
     esp_audio_codec_lib_add(player, AUDIO_CODEC_TYPE_DECODER, ogg_decoder_init(&ogg_dec_cfg));
 
     // Add i2s writer
     // Create writers and add to esp_audio
     ESP_LOGI(TAG, "Creating i2s writer...");
-    i2s_writer = I2S_STREAM_CFG_DEFAULT();
-    i2s_writer.i2s_config.bits_per_chan = I2S_BITS_PER_CHAN_16BIT;
-    i2s_writer.i2s_config.mclk_multiple = I2S_MCLK_MULTIPLE_DEFAULT;
+    i2s_writer = I2S_STREAM_CFG_DEFAULT();       
+    i2s_writer.i2s_config.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;        
     esp_audio_output_stream_add(player, i2s_stream_init(&i2s_writer));
 
     // Set default volume
@@ -135,5 +145,6 @@ void AudioController::stopPlaying() {
 }
 
 bool AudioController::isPlaying() {
-    player.
+    // TODO - find a method that will enable us to find out if the playing has finished
+    return true;
 }
