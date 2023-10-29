9dd991314d6310f50ec5796860c15cdfffc78b9c
Chris Woods <chris@cmwoods.com>
2023-10-29 12:51:25 +0100

    still problems with mcp23x17 -> other i2c devices fine

--- main/main.cpp
+++ main/main.cpp
@@ -78,8 +78,8 @@ void app_main() {
     esp_log_level_set("I2S_STREAM", ESP_LOG_WARN);
     esp_log_level_set("ReelController", ESP_LOG_DEBUG);
     esp_log_level_set("m20ly02z", ESP_LOG_WARN);
-    esp_log_level_set("i2c_manager", ESP_LOG_WARN);
-    esp_log_level_set("i2c", ESP_LOG_DEBUG);
+    esp_log_level_set("i2c_manager", ESP_LOG_VERBOSE);
+    esp_log_level_set("i2c", ESP_LOG_WARN);
     //esp_log_level_set("cctalkDevice", ESP_LOG_DEBUG);
     //esp_log_level_set("cctalk_link_controller", ESP_LOG_DEBUG);
     //esp_log_level_set("serial_worker", ESP_LOG_DEBUG);
