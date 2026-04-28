725f6425f214cc1c89e18d44e143acbb79f43291
Chris Woods <chris@cmwoods.com>
2026-04-28 20:26:38 +0200

    Added http controller and wifi controller with OTA update

---
+++ main/httpcontroller.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include <memory>
+
+#include "esp_http_server.h"
+#include "esp_err.h"
+
+#include "wifi_manager.hpp"
+
+class HttpController {
+public:
+    HttpController(WifiManager& wifi_manager);
+    ~HttpController();
+
+    esp_err_t start();
+    void stop();
+
+    // Call periodically to push live data to all WS clients
+    void broadcast_status(int uptime_sec, int heap_free);
+
+private:
+    static esp_err_t root_handler(httpd_req_t *req);
+    static esp_err_t asset_handler(httpd_req_t *req);
+    static esp_err_t provision_handler(httpd_req_t *req);
+    static esp_err_t api_status_handler(httpd_req_t *req);
+    static esp_err_t ws_handler(httpd_req_t *req);
+    static esp_err_t ota_upload_handler(httpd_req_t *req);
+
+    static esp_err_t send_file(httpd_req_t *req, const char *path);
+    static void ws_broadcast(httpd_handle_t server, int uptime_sec, int heap_free);
+
+    httpd_handle_t server;
+};
